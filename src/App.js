110a6c86af24fc0abbacd73bebd6e29d8a520365
yohanym95 <malshikay@gmail.com>
2020-05-18 19:08:29 +0530

    Create todo component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 import { Container } from 'react-bootstrap'
 import AddTodo from './components/addTodo'
+import Todo from './components/todo'
 
-function App() {
+function App () {
+  const [todo, setTodo] = useState({
+    todo: [
+      { title: 'Have to finish EA Assignment', id: 1 },
+      { title: 'Have to start flutter new project', id: 2 },
+      { title: 'Have to create blog app', id: 3 },
+      { title: 'There is birthday party in next week', id: 4 }
+    ]
+  })
   return (
-    <div className="App">
-       <Container>
-         <h1 className='display-1'>Todos</h1>
-         <AddTodo />
-       </Container>
+    <div className='App'>
+      <Container>
+        <h1 className='display-1'>Todos</h1>
+        <AddTodo />
+        <Todo todo={todo} />
+      </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/todo.js
@@ -0,0 +1,29 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import '../App.css'
+import { Container, Card, Row, Col } from 'react-bootstrap'
+
+function Todos (props) {
+  return (
+    <>
+      <Container className=' pt-2  text-dark '>
+        {props.todo.todo.map((todo, id) => (
+          <Card className='p-3 m-3 bg-light' key={id}>
+            <Row>
+              <Col className='col-10'>
+                <h4 className='text-justify display-4'>{todo.title}</h4>
+              </Col>
+              <Col className='col-2'>
+                <a className='btn text-danger align-middle'>
+                  <i className='fas fa-trash-alt' />
+                </a>
+              </Col>
+            </Row>
+          </Card>
+        ))}
+      </Container>
+    </>
+  )
+}
+
+export default Todos
