e3bbf31df73c25da2bb879b6e67b21ee25c2c4ab
yohanym95 <malshikay@gmail.com>
2020-05-18 18:49:43 +0530

    Create add todo component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Container } from 'react-bootstrap'
+import AddTodo from './components/addTodo'
 
 function App() {
   return (
     <div className="App">
-       <p>Hello World</p>
+       <Container>
+         <h1 className='display-1'>Todos</h1>
+         <AddTodo />
+       </Container>
     </div>
   );
 }
---
+++ src/components/addTodo.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import '../App.css'
+import { Container, Form, Button } from 'react-bootstrap'
+
+const AddTodo = () => {
+  const [input, setInput] = useState(null)
+
+  const formHandler = e => {
+    setInput(e.target.value)
+    console.log(e.target.value)
+  }
+
+  const submitHandler = e => {
+    e.preventDefault()
+    if (input.length > 3) {
+    } else {
+    }
+  }
+
+  return (
+    <>
+      <Container>
+        <Container>
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId='formBasicEmail'>
+              <Form.Control
+                type='text'
+                placeholder='Enter Your Todos'
+                className='p-4'
+                onChange={formHandler}
+              />
+            </Form.Group>
+
+            <Button
+              type='submit'
+              className='text-white font-weight-bold lg pl-5 pr-5'
+            >
+              Add Your Todos
+            </Button>
+          </Form>
+        </Container>
+      </Container>
+    </>
+  )
+}
+
+export default AddTodo
