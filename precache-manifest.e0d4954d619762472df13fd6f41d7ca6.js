87dd2e03b24e36836f69144ea8fd86adb93f5037
yohanym95 <malshikay@gmail.com>
2020-05-20 21:30:57 +0530

    Updates

---
+++ precache-manifest.e0d4954d619762472df13fd6f41d7ca6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1023b2a6186ee74d08fc7dcdfd11b377",
+    "url": "/React-todo/index.html"
+  },
+  {
+    "revision": "2a031584c4efcdb980a5",
+    "url": "/React-todo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "239004b8002ff2a1674b",
+    "url": "/React-todo/static/css/main.d950bfac.chunk.css"
+  },
+  {
+    "revision": "2a031584c4efcdb980a5",
+    "url": "/React-todo/static/js/2.c811de7f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-todo/static/js/2.c811de7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "239004b8002ff2a1674b",
+    "url": "/React-todo/static/js/main.9da4577e.chunk.js"
+  },
+  {
+    "revision": "fdfc7f7143bca6e61c7f",
+    "url": "/React-todo/static/js/runtime-main.f2fa4286.js"
+  }
+]);
